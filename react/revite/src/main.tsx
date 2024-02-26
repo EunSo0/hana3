bfa1ab4b2fb797ed0cba575957e024d6e7e08852
EunSo0 <dmstn423@naver.com>
2024-02-26 16:40:43 +0900

    router

--- react/revite/src/main.tsx
+++ react/revite/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx';
 import Sample from '../src/components/Sample.tsx';
 import './index.css';
 import { CounterProvider } from './contexts/counter-context.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CounterProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </CounterProvider>
     <Sample />
   </React.StrictMode>
